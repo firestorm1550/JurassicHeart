195a260eb3031b9ecc079e3796e538e4f7a9b5f2
Andrew Welby <apw50@pitt.edu>
2020-02-01 16:52:58 -0500

    aaaaaaaaaaaaaaaa

---
+++ Jurassic Heart/Assets/Scripts/Welby/DirtRemove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirtRemove : MonoBehaviour
+{
+	public GameObject particlesPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+	}
+
+	public void RemoveDirt() {
+		GameObject particleObj = Instantiate(particlesPrefab, transform.position, transform.rotation);
+		GameObject.Destroy(particleObj, 2f);
+		GameObject.DestroyImmediate(this);
+	}
+}
--- Jurassic Heart/Assets/Scripts/Welby/PlaceBonesInWorld.cs
+++ Jurassic Heart/Assets/Scripts/Welby/PlaceBonesInWorld.cs
@@ -16,6 +16,10 @@ public class PlaceBonesInWorld : MonoBehaviour {
 	[Tooltip("Instantiates the origin on a plane at the touch location.")]
 	GameObject m_PlacedOriginPrefab;
 
+	private bool isOriginSet = false;
+	List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+	ARRaycastManager m_RaycastManager;
+
 	/// <summary>
 	/// The prefab to instantiate on touch.
 	/// </summary>
@@ -56,20 +60,39 @@ public class PlaceBonesInWorld : MonoBehaviour {
 		if (!TryGetTouchPosition(out Vector2 touchPosition))
 			return;
 
-		if (m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon)) {
-			// Raycast hits are sorted by distance, so the first one
-			// will be the closest hit.
-			var hitPose = s_Hits[0].pose;
+		if (!isOriginSet) {
+			if (m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon)) {
+				// Raycast hits are sorted by distance, so the first one
+				// will be the closest hit.
+				var hitPose = s_Hits[0].pose;
 
-			if (spawnedObject == null) {
-				spawnedObject = Instantiate(m_PlacedOriginPrefab, hitPose.position, hitPose.rotation);
-			} else {
-				spawnedObject.transform.position = hitPose.position;
+				if (spawnedObject == null) {
+					spawnedObject = Instantiate(m_PlacedOriginPrefab, hitPose.position, hitPose.rotation);
+					isOriginSet = true;
+				} else {
+					spawnedObject.transform.position = hitPose.position;
+				}
+			}
+			
+		} else {
+			foreach (Touch touch in Input.touches) {
+				if (touch.phase == TouchPhase.Began) {
+					// Construct a ray from the current touch coordinates
+					Ray ray = Camera.main.ScreenPointToRay(touch.position);
+					RaycastHit hit;
+					if (Physics.Raycast(ray, out hit, Mathf.Infinity)) {
+						if (hit.transform.gameObject.tag == "Dirt") {
+							Debug.Log("Dug Dirt");
+							hit.transform.GetComponent<DirtRemove>().RemoveDirt();
+						}
+
+					}
+				}
 			}
 		}
 	}
 
-	static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
 
-	ARRaycastManager m_RaycastManager;
+
+	
 }
