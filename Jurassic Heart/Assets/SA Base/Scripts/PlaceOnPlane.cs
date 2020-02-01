0b211e16c3cf16243503ddd04f414443304e0591
Robert Kasper <firestorm1550@gmail.com>
2020-02-01 11:18:26 -0500

    SImple AR Setup

--- Jurassic Heart/Assets/SA Base/Scripts/PlaceOnPlane.cs
+++ Jurassic Heart/Assets/SA Base/Scripts/PlaceOnPlane.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
@@ -16,14 +15,12 @@ public class PlaceOnPlane : MonoBehaviour
 {
     [SerializeField]
     [Tooltip("Instantiates this prefab on a plane at the touch location.")]
-    Ship m_PlacedPrefab;
+    GameObject m_PlacedPrefab;
 
-    public ShipViewExploder shipViewExploder;
-    
     /// <summary>
     /// The prefab to instantiate on touch.
     /// </summary>
-    public Ship placedPrefab
+    public GameObject placedPrefab
     {
         get { return m_PlacedPrefab; }
         set { m_PlacedPrefab = value; }
@@ -32,13 +29,11 @@ public class PlaceOnPlane : MonoBehaviour
     /// <summary>
     /// The object instantiated as a result of a successful raycast intersection with a plane.
     /// </summary>
-    public Ship spawnedObject { get; private set; }
+    public GameObject spawnedObject { get; private set; }
 
-    private GameObject spawnedObjectHolder;
     void Awake()
     {
         m_RaycastManager = GetComponent<ARRaycastManager>();
-        spawnedObjectHolder = new GameObject();
     }
 
     bool TryGetTouchPosition(out Vector2 touchPosition)
@@ -50,9 +45,8 @@ public class PlaceOnPlane : MonoBehaviour
             touchPosition = new Vector2(mousePosition.x, mousePosition.y);
             return true;
         }
-        
 #else
-        if (Input.touchCount > 0 )
+        if (Input.touchCount > 0)
         {
             touchPosition = Input.GetTouch(0).position;
             return true;
@@ -63,49 +57,26 @@ public class PlaceOnPlane : MonoBehaviour
         return false;
     }
 
-    private float lastHitTime = 0;
     void Update()
     {
-        if (spawnedObject == null)
+        if (!TryGetTouchPosition(out Vector2 touchPosition))
+            return;
+
+        if (m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
         {
-            if (!TryGetTouchPosition(out Vector2 touchPosition))
-                return;
+            // Raycast hits are sorted by distance, so the first one
+            // will be the closest hit.
+            var hitPose = s_Hits[0].pose;
 
-            if (Time.time > lastHitTime + 1 &&
-                m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
+            if (spawnedObject == null)
             {
-
-                lastHitTime = Time.time;
-                // Raycast hits are sorted by distance, so the first one
-                // will be the closest hit.
-                var hitPose = s_Hits[0].pose;
-
-                if (spawnedObject == null)
-                {
-                    spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position + Vector3.up/2, Quaternion.Euler(Vector3.up));
-                    spawnedObject.transform.parent = spawnedObjectHolder.transform;
-                    shipViewExploder.ship = spawnedObject;
-                    shipViewExploder.Init();
-                }
-                else
-                {
-                    spawnedObject.transform.position = hitPose.position;
-                    spawnedObject.transform.rotation = Quaternion.Euler(Vector3.up);
-                }
-
-                StartCoroutine(GenericCoroutines.DoAfterFrames(() => { }, 2));
+                spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                spawnedObject.transform.position = hitPose.position;
             }
         }
-    }
-
-    public void RemoveSpawnedObject()
-    {
-        foreach (Transform transform1 in spawnedObjectHolder.transform)
-        {
-            Destroy(transform1.gameObject);
-        }
-        Destroy(spawnedObject.gameObject);
-        //spawnedObject = null;
     }
 
     static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
