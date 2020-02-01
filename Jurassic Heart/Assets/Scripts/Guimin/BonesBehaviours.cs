38a7290f7e46160f64d4b0539962de8192c97c63
Guimin-Ren <58438622+Guimin-Ren@users.noreply.github.com>
2020-02-01 17:34:09 -0500

    edit the script of bone behaviour

--- Jurassic Heart/Assets/Scripts/Guimin/BonesBehaviours.cs
+++ Jurassic Heart/Assets/Scripts/Guimin/BonesBehaviours.cs
@@ -10,6 +10,7 @@ public class BonesBehaviours : MonoBehaviour
     [SerializeField] private bool discoverActive = false;
     [SerializeField] private GameObject bone;
     private bool generated = false;
+    public bool continueGame = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,18 +21,27 @@ public class BonesBehaviours : MonoBehaviour
     void Update()
     {
         if(count >= maxDiscoverCount && !generated)
-        {         
+        {
+            StartCoroutine(StartMiniGame());
             Instantiate(bone, transform.position, transform.rotation);
             generated = true;
             Destroy(gameObject);
         }
-
-                
-       
 
     }
     public void Discover()
     {
         count++;
     }
+    public void BoneContinue()
+    {
+        continueGame = true;
+    }
+    IEnumerator StartMiniGame()
+    {
+        //startMinigame
+        yield return new WaitUntil(() => continueGame == true);
+    }
+
+
 }
---
+++ Jurassic Heart/Assets/StoryAsset/StoryManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] images;
+    [SerializeField] private GameObject[] dialogues;
+    public static StoryManager Instance = null;
+    private void OnEnable()
+    {
+        if (Instance != null)
+        {
+            Destroy(Instance);
+        }
+        Instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < images.Length; i++)
+        {
+            images[i].SetActive(false);
+        }
+        for (int i = 0; i < dialogues.Length; i++)
+        {
+            images[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartTellingStory()
+    {
+        StartCoroutine(Storytelling());
+    }
+    IEnumerator Storytelling()
+    {
+        images[0].SetActive(true);
+        yield return new WaitForSeconds(3f);
+        images[0].SetActive(false);
+        images[1].SetActive(true);
+        yield return new WaitForSeconds(3f);
+        images[1].SetActive(false);
+        images[2].SetActive(true);
+        yield return new WaitForSeconds(3f);
+        images[2].SetActive(false);
+        images[3].SetActive(true);
+        yield return new WaitForSeconds(3f);
+        images[3].SetActive(false);
+    }
+
+}
